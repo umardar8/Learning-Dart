7c545c42bae10bd9b473f20f7afbe50a02f3808b
umardar8 <umerfarooqdar.official@gmail.com>
2023-07-21 20:31:28 +0500

    another iteration of nested for loop for pyramids

--- pyramidNestedLoop.dart
+++ pyramidNestedLoop.dart
@@ -1,10 +1,19 @@
 import 'dart:io';
 void main() {
   var space = " ";
+  num counter = 0;
   for(var i = 1; i <= 5; i++) {
     stdout.write("${space * (5-i)}");
     for(var j = 1; j <=i; j++) {
       stdout.write("* ");
+    }
+    print("");
+  }
+
+  for(var k = 1; k < 5; k++) {
+    stdout.write("${space * (5-k)}");
+    for(var l = 1; l <=k; l++) {
+      stdout.write("${counter++} ");
     }
     print("");
   }
